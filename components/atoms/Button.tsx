c469b722f5c3e1cca42fc72682c7b7cf87a3efcd
田中尊 <tanakason@tanakasonnoMacBook-Pro.local>
2026-01-19 18:34:59 +0900

    fixadd

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -15,9 +15,9 @@ interface BtnProps {
 export default function Btn({ value, className ,onClick, disabled, type = "button", icon, ...props }: BtnProps) {
     return (
         <motion.button
-            whileHover={{ scale: 1.02, y: -5 }}
+            whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.95 }}
-            className={`text-black bg-white border border-dotted border-3 font-bold py-2 px-4 rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300 ${className}`}
+            className={`text-black bg-white border hover:bg-gray-200 transition duration-500 border-dotted border-3 font-bold py-2 px-4 rounded-2xl shadow-lg hover:shadow-2xl transition-shadow duration-300 ${className}`}
             onClick={onClick}
             disabled={disabled}
             type={type}
