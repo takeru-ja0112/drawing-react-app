a4847189b57ddd65da7b9822383003824a3ca205
takeru-ja0112 <takeruchandesu@gmail.com>
2026-01-27 10:48:59 +0900

    fix

--- hooks/usePushControle.ts
+++ hooks/usePushControle.ts
@@ -11,7 +11,7 @@ export default function usePushControl() {
     const registration = await navigator.serviceWorker.ready;
     const subscription = await registration.pushManager.subscribe({
       userVisibleOnly: true,
-      applicationServerKey: process.env.VAPID_PUBLIC_KEY!
+      applicationServerKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!
     });
     setSub(subscription); // 取得した情報をステートに保存
     console.log("Subscribed:", subscription);
