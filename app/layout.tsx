86bf4abf8ced90ca5872001c5886782c78a8bfac
takeru <takeruchandesu@gmail.com>
2026-01-29 23:50:20 +0900

    fix:soundSystem

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 // "use client";
 // import { useEffect } from "react";
 
-import type { Metadata } from "next";
 import BgObject from "@/components/organisms/BgObject";
 import Header from "@/components/organisms/Header";
-import { SoundProvider } from "@/components/SoundProvider";
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -55,14 +54,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-b from-gray-100 to-gray-200 min-h-screen`}
       >
         <BgObject />
-        <SoundProvider>
           {/* ヘッダーの高さ分全体をさげる */}
           <Header />
           <div className="pt-14">{children}</div>
           <footer className="text-center p-4 text-gray-500 text-sm">
             &copy; 2026, Takeru
           </footer>
-        </SoundProvider>
       </body>
     </html>
   );
