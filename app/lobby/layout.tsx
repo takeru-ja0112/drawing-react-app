8ea99bde95d03bac5cfa1761c5c052cac8b224c7
takeru <takeruchandesu@gmail.com>
2026-01-31 16:52:27 +0900

    fix

--- app/lobby/layout.tsx
+++ app/lobby/layout.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
 
 export default function Layout(
   {
@@ -9,14 +6,14 @@ export default function Layout(
     Readonly<{ children: React.ReactNode; }>
 ) {
 
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/custom-sw.js")
-        .then((reg) => { console.log("SW registered!", reg); })
-        .catch((err) => { console.error("SW registration failed!", err); alert("Service Worker登録に失敗しました。プッシュ通知は利用できません。"); });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if ("serviceWorker" in navigator) {
+  //     navigator.serviceWorker
+  //       .register("/custom-sw.js")
+  //       .then((reg) => { console.log("SW registered!", reg); })
+  //       .catch((err) => { console.error("SW registration failed!", err); alert("Service Worker登録に失敗しました。プッシュ通知は利用できません。"); });
+  //   }
+  // }, []);
 
   return (
     <>
--- app/room/[id]/layout.tsx
+++ app/room/[id]/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ModalProvider } from "@/hooks/useModalContext";
+import { useEffect } from "react";
 
 export default function Layout(
   {
@@ -8,6 +9,15 @@ export default function Layout(
   }:
     Readonly<{ children: React.ReactNode; }>
 ) {
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/custom-sw.js")
+        .then((reg) => { console.log("SW registered!", reg); })
+        .catch((err) => { console.error("SW registration failed!", err); alert("Service Worker登録に失敗しました。プッシュ通知は利用できません。"); });
+    }
+  }, []);
 
   return (
     <ModalProvider>
